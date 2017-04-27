ca7c64b26b412b4e80009fb55fef7de5cc1cb9d0
Alex Sumak <alex.sumak@Alex-Sumaks-MacBook-Air.local>
2017-04-26 18:58:03 -0700

    Initial Commit

---
+++ Tinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tinder
+//
+//  Created by  Alex Sumak on 4/26/17.
+//  Copyright © 2017  Alex Sumak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
